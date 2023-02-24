e56c5c7b1a89bddfb08ac4ebcd00b8a83bfe8572
Rafaël Carré <funman@videolan.org>
2023-02-24 15:16:24 +0100

    change script

--- run.sh
+++ run.sh
@@ -2,6 +2,6 @@
 
 set -e
 
-export FI_LOG_LEVEL=10
+#export FI_LOG_LEVEL=10
 
-(while :;do cat f;done)| exec ./tx 192.168.0.221 192.168.0.177:47593
+(while :;do cat f;done)| exec ./tx 10.0.0.2 10.0.0.1:3002
